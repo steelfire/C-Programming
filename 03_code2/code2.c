97f9526d110d793dc817dbddd72981f9d8056ff3
Coursera Learner <nobody@nowhere.nul>
2018-05-27 07:57:25 +0000

    did assignment 4

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -1,5 +1,17 @@
 
 int printTriangle(int size) {
+  int starCount = 0;
+  for (i = 0; i < size; i++) {
+    for (j = 0; j <= i; j++) {
+    printf("*");
+    starCount++;
+  }
+    printf("\n");
+  }
+  return starCount;
+}
+	  
+	   
   //start with starCount being 0
 
   //count from 0 (inclusive) to size (exclusive), for each number i that you count
@@ -25,6 +37,9 @@ int main(void) {
   int numStars;
   printf("Here is a triangle with height 4\n");
   numStars = printTriangle(4);
+  printf("That triangle had %d total stars\n", numStars);
+  printf("Here is a triangle with height 7\n");
+  numStars = printTriangle(7);
   printf("That triangle had %d total stars\n", numStars);
   //now print "Here is a triangle with height 7\n"
 
